4246129523735cd67b58ea985fcc77ff7f1b82a2
Martin Pecka <peci1@seznam.cz>
2013-03-19 17:29:59 +0100

    Improved JFileInput action firing.

--- AIS-Data-Reader/src/main/java/javax/swing/peckam/JFileInput.java
+++ AIS-Data-Reader/src/main/java/javax/swing/peckam/JFileInput.java
@@ -7,6 +7,8 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.awt.event.MouseEvent;
 import java.io.File;
 
@@ -99,12 +101,21 @@ public class JFileInput extends JComponent
             }
         });
 
-        textField.addActionListener(new ActionListener() {
+        final ActionListener pathChangeAction = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e)
             {
                 fileChooser.setCurrentDirectory(new File(textField.getText()));
                 fireActionPerformed();
+            }
+        };
+
+        textField.addActionListener(pathChangeAction);
+        textField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e)
+            {
+                pathChangeAction.actionPerformed(null);
             }
         });
     }
